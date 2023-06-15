170ef56d4e1dab4e4bdc8678b2bca726ae531bd8
Niel de Wet <nieldw@users.noreply.github.com>
2023-06-15 21:49:50 +0200

    Create NodeJS script to stake all SOL and AVAX in VALR account
    Just supply an API key/secret with View and Trade permissions as environment variables, API_KEY and API_SECRET

---
+++ stake-all.js
@@ -0,0 +1,70 @@
+const crypto = require('crypto');
+
+const apiKey = process.env.API_KEY
+const apiSecret = process.env.API_SECRET
+const stakingCurrencies = ['SOL', 'AVAX']
+
+function signRequest(apiSecret, timestamp, verb, path, body = '') {
+    return crypto
+        .createHmac("sha512", apiSecret)
+        .update(timestamp.toString())
+        .update(verb.toUpperCase())
+        .update(path)
+        .update(body)
+        .digest("hex");
+}
+
+function getHeaders(timestamp, verb, path, body = '') {
+    return {
+        "Content-type": "application/json; charset=UTF-8",
+        "X-VALR-API-KEY": `${apiKey}`,
+        "X-VALR-SIGNATURE": `${signRequest(apiSecret, timestamp, verb, path, body)}`,
+        "X-VALR-TIMESTAMP": `${timestamp}`
+    };
+}
+
+function stakeAll(currency, amount) {
+    console.log(`Staking ${amount} ${currency}`)
+    let timestamp = (new Date()).getTime()
+    const body = `{"currencySymbol":"${currency}","amount":"${amount}"}`;
+    const options = {
+        method: "POST",
+        body: body,
+        headers: getHeaders(timestamp, "POST", "/v1/staking/stake", body)
+    };
+    return fetch('https://api.valr.com/v1/staking/stake', options)
+}
+
+async function getCurrencies() {
+    let timestamp = (new Date()).getTime()
+    let options = {
+        method: "GET",
+        headers: getHeaders(timestamp, "GET", "/v1/public/currencies"),
+    };
+    return fetch('https://api.valr.com/v1/public/currencies', options)
+        .then((response) => response.json())
+        .then((json) => json.filter(currency => stakingCurrencies.includes(currency.shortName)))
+        .then((json) => json)
+}
+
+getCurrencies().then((currencies) => {
+    let timestamp = (new Date()).getTime()
+    let options = {
+        method: "GET",
+        headers: getHeaders(timestamp, "GET", "/v1/account/balances?excludeZeroBalances=true"),
+    };
+    return fetch('https://api.valr.com/v1/account/balances?excludeZeroBalances=true', options)
+        .then((response) => response.json())
+        .then((json) => json.filter(balance => stakingCurrencies.includes(balance.currency)))
+        .then((array) => array.forEach((balance) => {
+            const currencyDecimalPlaces = currencies.find((currency) => currency.shortName === balance.currency).withdrawalDecimalPlaces
+            const available = `${balance.available}`;
+            const toStake = available.substring(0, available.indexOf('.') + Number(currencyDecimalPlaces) + 1)
+            if (Number(toStake) > 0) {
+                return stakeAll(balance.currency, toStake)
+            } else {
+                return console.log(`Insufficient balance to stake: ${available} ${balance.currency}`)
+            }
+        }))
+}).then((response) => {
+}, (failure) => console.log(failure))
